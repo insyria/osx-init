03323bdb899a3334dbdd9700445989e6e85fa71b
insyria <insyria@gmail.com>
2019-08-17 19:05:31 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+# file taken originally from https://raw.githubusercontent.com/chcokr/osx-init/master/install.sh
+# install Xcode Command Line Tools
+# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+brew install \
+  git \
+  nmap \
+  openssl \
+  python \
+  redis \
+;
+brew tap caskroom/cask;
+brew cask install \
+  dropbox \
+  firefox \
+  google-chrome \
+  java \
+  vlc \
+;
